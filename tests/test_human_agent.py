bac50b16b38578d83c9d5a8831fe2fdfd0c9b4d4
Ewan Miller <ewan.miller@kcl.ac.uk>
2024-12-27 10:38:53 +0100

    update tests, add new human ui test

---
+++ tests/test_human_agent.py
@@ -0,0 +1,172 @@
+# Python stuff
+import numpy as np
+import cProfile
+import time
+import random
+
+# tensorflow stuff
+import tensorflow as tf
+from tf_agents.environments import tf_py_environment
+from tf_agents.trajectories import time_step as ts
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from tf_agents.utils import common
+from tf_agents import environments
+
+# cardshark stuff
+import cardshark
+from cardshark import logging as log
+from cardshark.agent import MultiAgent
+
+from examples.coup import coup_engine, coup_player, coup_agent
+
+nPlayers = 4
+
+## Build the environments for training and evaluation
+train_logger = log.Logger(name = "Train_env_Logger", logLevel = log.logLevels.kInfo, toFile = False)
+train_py_env = coup_engine.CoupGame(nPlayers = nPlayers, unravelActionSpace=True, logger = train_logger, maxSteps=250)
+batched_train_py_env = environments.BatchedPyEnvironment([train_py_env])
+train_env = tf_py_environment.TFPyEnvironment(batched_train_py_env, check_dims=True)
+
+eval_logger = log.Logger(name = "Eval_env_Logger", logLevel = log.logLevels.kInfo, toFile = False)
+eval_py_env = coup_engine.CoupGame(nPlayers = nPlayers, unravelActionSpace=True, logger = eval_logger, maxSteps=250)
+batched_eval_py_env = environments.BatchedPyEnvironment([eval_py_env])
+eval_env = tf_py_environment.TFPyEnvironment(batched_eval_py_env, check_dims=True)
+
+def runMatchSingleHuman(human_agent, robot_agents, collect=False, random=False, train=True):
+    """ run a match and train agent """
+    ## TODO: add random collection policy to the MultiAgent that can be used to randomly sample environment
+    step = train_env.reset()
+
+    while(step.step_type != ts.StepType.LAST):
+
+        ## First player is played by the human
+        if step.observation["activePlayer"] == 0:
+
+            action = human_agent.get_action()
+            step = train_env.step(action)
+
+        ## for all others we use a random policy
+        else:
+
+            ## First player is played by the actual agent
+            active_agent = step.observation["activePlayer"][0]
+
+            agent = robot_agents[active_agent]
+        
+            ## check whether or not we should store this frame
+            save_frame = (not train_env.get_info()["skippingTurn"]) or (train_env.get_info()["reward"] != 0.0)
+
+            agent._set_current_step(step)
+
+            if save_frame:
+                agent.add_frame()
+
+            action = agent.get_action(step, collect=collect, random=random)
+            step = train_env.step(action)
+            
+            if train: 
+                agent.train_agent()
+
+        if(train_py_env.player_list[0].isAlive):
+            time.sleep(3)
+        else:
+            time.sleep(0.25)
+
+
+def runMatchMultiAgent(agents, train: bool, collect: bool = False, random: bool = False):
+    """ run a match and train agent """
+    ## TODO: add random collection policy to the MultiAgent that can be used to randomly sample environment
+    
+    assert len(agents) == nPlayers
+    
+    step = train_env.reset()
+
+    while(step.step_type != ts.StepType.LAST):
+
+        ## First player is played by the actual agent
+        active_agent = step.observation["activePlayer"][0]
+
+        agent = agents[active_agent]
+    
+        ## check whether or not we should store this frame
+        save_frame = (not train_env.get_info()["skippingTurn"]) or (train_env.get_info()["reward"] != 0.0)
+
+        agent._set_current_step(step)
+
+        if save_frame:
+            agent.add_frame()
+
+        action = agent.get_action(step, collect=collect, random=random)
+        step = train_env.step(action)
+        
+        if train: 
+            agent.train_agent()
+
+
+human_agent = coup_agent.CoupHumanAgent(name = coup_agent.MAGENTA + "Kyle" + coup_agent.RESET)
+human_agent.register_game(train_py_env, 0)
+train_py_env.player_list[0].name = human_agent.name
+
+## generate some random colours for the bots
+bot_colours =[] 
+for i in range(nPlayers - 1):
+    bot_colours.append("\u001b[38;5;{}m".format(random.randint(21, 230)))
+    train_py_env.player_list[1 + i].name = bot_colours[i] + "bot_" + str(i) + coup_agent.RESET
+
+## Load up some (possibly pre-trained) bots to play against the human
+robot_agents = []
+for i in range(nPlayers):
+    robot_agents.append(
+        MultiAgent(
+            train_env.time_step_spec(), 
+            train_env.action_spec(), 
+            train_batch_size = 64,
+            observation_spec = train_env.time_step_spec().observation["observations"],
+            checkpoint=False,
+            DqnAgent_args={
+                "optimizer": tf.keras.optimizers.Adam(learning_rate=0.5e-2),
+                "epsilon_greedy": 0.4},
+            fc_layer_params=(100,100,100),
+            logger=train_py_env.logger,
+            name = "bot_" + str(i)
+        )
+    )
+
+
+def trainMultiAgent(agents, nMatches, plotInterval):
+    
+    step = train_env.reset()
+    
+    for collectMatchIndex in range(5):
+        log.INFO("######## Collect Match {} ##########".format(collectMatchIndex))
+        runMatchMultiAgent(agents, train = False, random = True)
+        
+    for matchIndex in range(1,nMatches):
+        log.INFO("######## Train Match {} ##########".format(matchIndex))
+        runMatchMultiAgent(agents, train = True, collect = True)
+
+        if(matchIndex % plotInterval) == 0:
+            for agent in agents:
+                agent.plot_training_losses()
+                agent.plot_rewards()
+
+# train the bots a little bit first
+train_loggerlogLevek = log.logLevels.kSilent ## sshhhh
+trainMultiAgent(robot_agents, 5, 4)
+
+train_logger.logLevel = log.logLevels.kInfo
+while True:
+    runMatchSingleHuman(human_agent, robot_agents)
+
+    play_again = input("Play again? [Y/N]")
+    
+    if play_again in ["Y", "y", "Yes", "yes"]:
+        continue
+    elif play_again in ["N", "n", "No", "no"]:
+        break
+    else:
+        print("Invalid option")
+        play_again = input("Play again? [Y/N]")
+
+for agent in robot_agents:
+    agent.plot_training_losses()
--- tests/test_training.py
+++ tests/test_training.py
@@ -13,8 +13,7 @@ from tf_agents import environments
 # Coup AI stuff
 import cardshark
 from cardshark import logging as log
-from cardshark.multi_agent import MultiAgent
-from cardshark.engine import Game
+from cardshark.agent import MultiAgent
 
 from examples.coup import coup_engine, coup_player
 
@@ -26,6 +25,7 @@ log.INFO('Physical Devices:\n', tf.config.list_physical_devices(), '\n\n')
 
 nPlayers = 4
 
+## Build the environments for training and evaluation
 train_logger = None #log.Logger(name = "Train_env_Logger", logLevel = log.logLevels.kInfo, toFile = True)
 train_py_env = coup_engine.CoupGame(nPlayers = nPlayers, unravelActionSpace=True, logger = train_logger, maxSteps=250)
 batched_train_py_env = environments.BatchedPyEnvironment([train_py_env])
@@ -36,35 +36,19 @@ eval_py_env = coup_engine.CoupGame(nPlayers = nPlayers, unravelActionSpace=True,
 batched_eval_py_env = environments.BatchedPyEnvironment([eval_py_env])
 eval_env = tf_py_environment.TFPyEnvironment(batched_eval_py_env, check_dims=True)
 
-## Train a single agent among random agents
-agent = MultiAgent(
-        train_env.time_step_spec(), 
-        train_env.action_spec(), 
-        train_batch_size = 64,
-        observation_spec = train_env.time_step_spec().observation["observations"],
-        checkpoint=True,
-        DqnAgent_args={
-            "optimizer": tf.keras.optimizers.Adam(learning_rate=0.5e-2),
-            "epsilon_greedy": 0.4},
-        fc_layer_params=(100,100,100),
-        logger=train_py_env.logger,
-    )
-
-agent.save_checkpoint()
-
-random_policy = RandomTFPolicy(
-        eval_env.time_step_spec(), 
-        eval_env.action_spec(), 
-        observation_and_action_constraint_splitter = agent._obs_constraint_splitter()
-    )
-
-## wrap up the action to speed things up
-random_action = common.function(random_policy.action)
-
-def runMatchSingleAgent(train: bool, collect: bool = False, random: bool = False):
+def runMatchSingleAgent(agent, train: bool, collect: bool = False, random: bool = False):
     """ run a match and train agent """
     ## TODO: add random collection policy to the MultiAgent that can be used to randomly sample environment
     step = train_env.reset()
+
+    random_policy = RandomTFPolicy(
+            eval_env.time_step_spec(), 
+            eval_env.action_spec(), 
+            observation_and_action_constraint_splitter = agent._obs_constraint_splitter()
+        )
+
+    ## wrap up the action to speed things up
+    random_action = common.function(random_policy.action)
 
     while(step.step_type != ts.StepType.LAST):
 
@@ -77,9 +61,9 @@ def runMatchSingleAgent(train: bool, collect: bool = False, random: bool = False
             agent._set_current_step(step)
 
             if save_frame:
-                agent._add_frame()
+                agent.add_frame()
 
-            action = agent._get_action(step, collect=collect, random=random)
+            action = agent.get_action(step, collect=collect, random=random)
             step = train_env.step(action)
             
             if train: 
@@ -90,24 +74,123 @@ def runMatchSingleAgent(train: bool, collect: bool = False, random: bool = False
             action = random_action(step)
             step = train_env.step(action)
 
-def trainSingleAgent(nMatches, plotInterval):
+def trainSingleAgent(agent, nMatches, plotInterval):
     
     step = train_env.reset()
     
     for collectMatchIndex in range(5):
         log.INFO("######## Collect Match {} ##########".format(collectMatchIndex))
-        runMatchSingleAgent(train = False, random = True)
+        runMatchSingleAgent(agent, train = False, random = True)
         
     for matchIndex in range(1,nMatches):
         log.INFO("######## Train Match {} ##########".format(matchIndex))
-        runMatchSingleAgent(train = True, collect = True)
+        runMatchSingleAgent(agent, train = True, collect = True)
 
         if(matchIndex % plotInterval) == 0:
             agent.plot_training_losses()
             agent.plot_rewards()
 
+## Train a single agent among random agents
+single_agent = MultiAgent(
+        train_env.time_step_spec(), 
+        train_env.action_spec(), 
+        train_batch_size = 64,
+        observation_spec = train_env.time_step_spec().observation["observations"],
+        checkpoint=True,
+        DqnAgent_args={
+            "optimizer": tf.keras.optimizers.Adam(learning_rate=0.5e-2),
+            "epsilon_greedy": 0.4},
+        fc_layer_params=(100,100,100),
+        logger=train_py_env.logger,
+    )
 
-cProfile.run("trainSingleAgent(50, 50)", filename = "train-profile", sort= 2)
-agent.save_checkpoint()
-agent.plot_training_losses()
-agent.plot_rewards()
+#single_agent.save_checkpoint()
+
+#cProfile.run("trainSingleAgent(single_agent, 50, 50)", filename = "train-profile", sort= 2)
+#single_agent.save_checkpoint()
+#single_agent.plot_training_losses()
+#single_agent.plot_rewards()
+
+
+
+
+
+
+
+
+
+
+########################################
+## Now train multiple agents at once! ##
+########################################
+
+def runMatchMultiAgent(agents, train: bool, collect: bool = False, random: bool = False):
+    """ run a match and train agent """
+    ## TODO: add random collection policy to the MultiAgent that can be used to randomly sample environment
+    
+    assert len(agents) == nPlayers
+    
+    step = train_env.reset()
+
+    while(step.step_type != ts.StepType.LAST):
+
+        ## First player is played by the actual agent
+        active_agent = step.observation["activePlayer"][0]
+
+        agent = agents[active_agent]
+    
+        ## check whether or not we should store this frame
+        save_frame = (not train_env.get_info()["skippingTurn"]) or (train_env.get_info()["reward"] != 0.0)
+
+        agent._set_current_step(step)
+
+        if save_frame:
+            agent.add_frame()
+
+        action = agent.get_action(step, collect=collect, random=random)
+        step = train_env.step(action)
+        
+        if train: 
+            agent.train_agent()
+
+def trainMultiAgent(agents, nMatches, plotInterval):
+    
+    step = train_env.reset()
+    
+    for collectMatchIndex in range(5):
+        log.INFO("######## Collect Match {} ##########".format(collectMatchIndex))
+        runMatchMultiAgent(agents, train = False, random = True)
+        
+    for matchIndex in range(1,nMatches):
+        log.INFO("######## Train Match {} ##########".format(matchIndex))
+        runMatchMultiAgent(agents, train = True, collect = True)
+
+        if(matchIndex % plotInterval) == 0:
+            agent.plot_training_losses()
+            agent.plot_rewards()
+
+## Train a single agent among random agents
+agent_list = []
+for i in range(nPlayers):
+    agent_list.append(
+        MultiAgent(
+            train_env.time_step_spec(), 
+            train_env.action_spec(), 
+            train_batch_size = 64,
+            observation_spec = train_env.time_step_spec().observation["observations"],
+            checkpoint=True,
+            DqnAgent_args={
+                "optimizer": tf.keras.optimizers.Adam(learning_rate=0.5e-2),
+                "epsilon_greedy": 0.4},
+            fc_layer_params=(100,100,100),
+            logger=train_py_env.logger,
+            name = "bot_" + str(i)
+        )
+    )
+
+trainMultiAgent(agent_list, 500, 500)
+for agent in agent_list:
+    agent.save_checkpoint()
+    agent.plot_training_losses()
+    agent.plot_rewards()
+    
